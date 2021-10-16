c8966fb9f76eba3eb9a2f004a47df79d531a064c
Sérgio Sobrinho <sobrinhosergio00@gmail.com>
2021-10-16 18:24:52 -0300

    first commit

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const Footer: React.FC = () => {
+  return <Container>Interface feita por Sérgio Sobrinho</Container>;
+};
+
+export default Footer;
